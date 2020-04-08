cace6493d2e2cebb665030e19a11bc05dff076dd
Alexandra Rhodes <anrhodes5738@gmail.com>
2020-04-08 18:40:18 -0500

    modified ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,63 +6,35 @@ class RingBuffer:
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
-    
+
     def append(self, item):
-        if self.storage.length < self.capacity:
+        # check to see if list is at capacity
+        # if not ->
+        if self.capacity > self.storage.length:
+            # add item to tail
             self.storage.add_to_tail(item)
-            self.current = self.storage.head
-        elif self.storage.length == self.capacity:
-            old_node = self.storage.head
-            self.storage.remove_from_head()
-            self.storage.add_to_tail(item)
-            if old_node == self.current:
-                self.current = self.storage.tail
-    
-    
+            # if an item is already there, then replace it with current value
+            if self.storage.length == 1:
+                self.current = self.storage.head
+        else:
+            self.current.value = item
+            
+            if self.current is not self.storage.tail:
+                self.current = self.current.next
+            else:
+                self.current = self.storage.head
+
+
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-        
-        if self.storage.length == 0:
-            return 'buffer is empty'
 
-        node = self.current
-        list_buffer_contents.append(node.value)
+        node = self.storage.head
+        while node:
+            list_buffer_contents.append(node.value)
+            node = node.next
 
-        if node.next:
-            next_node = node.next
-        else:
-            next_node = self.storage.head
-
-        while next_node != node:
-            list_buffer_contents.append(next_node.value)
-            if next_node.next:
-                next_node = next_node.next
-            else:
-                next_node = self.storage.head
-
-        
         return list_buffer_contents
-
-buffer = RingBuffer(3)
-
-print(buffer.get())   # should return []
-
-buffer.append('a')
-buffer.append('b')
-buffer.append('c')
-
-print(buffer.get())   # should return ['a', 'b', 'c']
-
-# 'd' overwrites the oldest value in the ring buffer, which is 'a'
-buffer.append('d')
-
-print(buffer.get())   # should return ['d', 'b', 'c']
-
-buffer.append('e')
-buffer.append('f')
-
-print(buffer.get())   # should return ['d', 'e', 'f']
 
 # ----------------Stretch Goal-------------------
 
